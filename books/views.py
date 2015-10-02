22610473b122b726b1296dcae29eabc1ef4fb1a4
Sana Javed <sanajaved7@gmail.com>
2015-10-01 22:22:26 -0400

    Added form to add books to app

--- books/views.py
+++ books/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Book
+from .forms import BookForm
 
 def index(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            form = BookForm()
+    else:
+        form = BookForm()
     books = Book.objects.all()
-    return render(request, 'index.html', context={'books':books})
+    return render(request, 'index.html', context={'books':books, 'form':form})
