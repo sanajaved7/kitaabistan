80a429bab1c89a46c2239b647e60fc4a5690a8a2
Sana Javed <sanajaved7@gmail.com>
2015-10-12 23:23:15 -0400

    Added documentation to forms

--- books/forms.py
+++ books/forms.py
@@ -2,6 +2,8 @@ from django.forms import ModelForm
 from .models import Book
 
 class BookForm(ModelForm):
+    """ Creates a form with the fields from the Book
+    model. """
     class Meta:
         model = Book
         fields = "__all__"
