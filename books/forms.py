da8b2c327040063211fe54aeb49ff9238ce14591
Sana Javed <sanajaved7@gmail.com>
2015-10-28 22:28:55 -0400

    Updated BookForm to include and connect to new Author model

--- books/forms.py
+++ books/forms.py
@@ -1,9 +1,12 @@
-from django.forms import ModelForm
-from .models import Book
+from django import forms
+from .models import Book, Author
 
-class BookForm(ModelForm):
+class BookForm(forms.Form):
     """ Creates a form with the fields from the Book
     model. """
-    class Meta:
-        model = Book
-        fields = "__all__"
+    title = forms.CharField(label="Title", max_length=100)
+    author = forms.ModelChoiceField(queryset=Author.objects.all(), label="Existing Authors", required=False)
+    new_author = forms.CharField(label="New Author", max_length=100, required=False)
+    genre = forms.CharField(label="Genre", max_length=75)
+    owned = forms.BooleanField(label="Do you own this book?", required=False)
+    to_read = forms.BooleanField(label="Do you want to read this book?", required=False)
