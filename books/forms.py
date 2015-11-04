356f257fd13b24178a249e4a1ca228cdbe65eb6d
Sana Javed <sanajaved7@gmail.com>
2015-11-04 18:00:56 -0500

    Created User registration form

--- books/forms.py
+++ books/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.models import User
 from .models import Book, Author
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
 
 class BookForm(forms.Form):
     """ Creates a form with the fields from the Book
