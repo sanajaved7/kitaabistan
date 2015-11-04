e009ff61928dce8cf080c273dc4e182ee1655734
Sana Javed <sanajaved7@gmail.com>
2015-11-04 18:58:57 -0500

    Fixed broken migrations

--- books/migrations/0001_initial.py
+++ books/migrations/0001_initial.py
@@ -2,27 +2,38 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('first_name', models.CharField(max_length=30)),
+                ('last_name', models.CharField(max_length=30)),
+            ],
+        ),
         migrations.CreateModel(
             name='Book',
             fields=[
                 ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('title', models.CharField(max_length=100)),
-                ('author', models.CharField(max_length=100)),
                 ('genre', models.CharField(max_length=75)),
                 ('owned', models.BooleanField()),
                 ('to_read', models.BooleanField()),
+                ('author', models.ForeignKey(to='books.Author')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.AlterUniqueTogether(
             name='book',
-            unique_together=set([('title', 'author')]),
+            unique_together=set([('title', 'author', 'user')]),
         ),
     ]
