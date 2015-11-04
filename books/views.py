1e701841c3069974d17f643f83fc519668155aeb
Sana Javed <sanajaved7@gmail.com>
2015-11-04 18:04:02 -0500

    Created views for user to register, login, and logout.

--- books/views.py
+++ books/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book, Author
-from .forms import BookForm
+from .forms import BookForm, UserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+        else:
+            print(user_form.errors)
+    else:
+        user_form = UserForm()
+    return render(request, 'register.html', {'user_form': user_form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect('index')
+            else:
+                return HttpResponse("Account is disabled. Sorry!")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse('Invalid login details supplied.')
+    else:
+        return render(request, 'login.html', {})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('index')
 
 def clean_author(new_author):
     new_author = new_author.split(" ")
@@ -29,5 +70,5 @@ def index(request):
             form = BookForm()
     else:
         form = BookForm()
-    books = Book.objects.all()
+    books = Book.objects.filter(user=request.user.id)
     return render(request, 'index.html', context={'books':books, 'form':form})
