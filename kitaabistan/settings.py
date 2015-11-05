ce837f3ed2327383bc244cfe9a4d4acefd6e1d96
Sana Javed <sanajaved7@gmail.com>
2015-11-04 19:45:08 -0500

    Updating static file settings

--- kitaabistan/settings.py
+++ kitaabistan/settings.py
@@ -102,13 +102,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'app_static')
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'app_static')
+# STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', 'static_root'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', 'static_root'),)
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
