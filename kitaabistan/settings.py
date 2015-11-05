c87101ded144685f2f9cb2457c14c8daf528c4eb
Sana Javed <sanajaved7@gmail.com>
2015-11-04 19:12:39 -0500

    Configuring database to work with environment variables

--- kitaabistan/settings.py
+++ kitaabistan/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,11 +76,10 @@ WSGI_APPLICATION = 'kitaabistan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 
