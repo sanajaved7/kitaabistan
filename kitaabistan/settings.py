67205375ffb3a8b28976a0ce6bc115295c709474
Sana Javed <sanajaved7@gmail.com>
2015-11-04 19:32:05 -0500

    Adding Heroku settings

--- kitaabistan/settings.py
+++ kitaabistan/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -26,7 +28,7 @@ SECRET_KEY = 'ao846y3z%=+dze4dh9(0uqyf1jyc9l_od&u^ot#$#2tg7d%pa*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -100,10 +102,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'app_static')
+STATIC_ROOT = 'staticfiles'
+
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'app_static')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', 'static_root'),)
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', 'static_root'),
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
