5e08046df9024310f657edd7456c9ea79ab05aae
Sana Javed <sanajaved7@gmail.com>
2015-11-30 18:23:05 -0500

    Set up tests and created first test to retrieve saved book

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Author, Book
 
-# Create your tests here.
+class BookTest(TestCase):
+    ''' Testing Book model '''
+    def setUp(self):
+        self.user = User.objects.create_user(username = 'momo',
+            email='momo@momo.com',
+            password='momo')
+        self.author = Author.objects.create(
+            first_name='Test',
+            last_name='Author')
+        self.test_book = Book(
+            title="Test Book",
+            author=self.author,
+            user=self.user,
+            genre="Test Genre",
+            owned=True,
+            to_read=False
+            )
+        self.test_book.save()
+
+    def test_book_retrieval(self):
+        '''Tests whether we can retrieve an added book.'''
+        retrieved_book = Book.objects.get(title="Test Book")
+        self.assertEqual(retrieved_book.user.username, 'momo')
+        self.assertEqual(retrieved_book.user.email, 'momo@momo.com')
+        self.assertEqual(retrieved_book.author.first_name, "Test")
+        self.assertEqual(retrieved_book.author.last_name, "Author")
+        self.assertEqual(retrieved_book.genre, "Test Genre")
+        self.assertEqual(retrieved_book.owned, True)
+        self.assertEqual(retrieved_book.to_read, False)
+
+
